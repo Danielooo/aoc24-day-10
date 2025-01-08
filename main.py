469c806774ccef4255d9e4bc971518abb44b5e32
Danielooo <19654989+Danielooo@users.noreply.github.com>
2025-01-08 18:49:18 +0100

    check_up doet niks op 6,6

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ amount_found_trails = 0
 
 current_pos_trail_pos = None
 current_pos_trail_altitude = 0
+current_pos_junction = None
+current_junction_left_over_directions = []
 
 
 def read_file_to_grid(filename):
-    with open (filename, 'r') as file:
+    with open(filename, 'r') as file:
         lines = file.readlines()
 
     grid = [[int(char) if char.isdigit() else char for char in line.strip()] for line in lines]
@@ -35,12 +37,11 @@ def check_up():
     global current_pos_trail_pos
     global current_pos_trail_altitude
     y, x = current_pos_trail_pos
-    if y <= 0 and grid[y][x + 1] != '.':
+    print(grid[y - 1][x])
+    if y > 0 and grid[y - 1][x] != '.':
         if grid[y - 1][x] == current_pos_trail_altitude + 1:
             current_pos_trail_pos = [y - 1, x]
             current_pos_trail_altitude += 1
-
-
 
 
 def check_right():
@@ -63,7 +64,6 @@ def check_down():
             current_pos_trail_altitude += 1
 
 
-
 def check_left():
     global current_pos_trail_pos
     global current_pos_trail_altitude
@@ -74,12 +74,65 @@ def check_left():
             current_pos_trail_altitude += 1
 
 
+def is_junction_test():
+    amount_of_directions = 0
+    next_altitude = current_pos_trail_altitude + 1
+    # UP
+    if y > 0 and grid[y - 1][x] == next_altitude:
+        amount_of_directions += 1
+    # RIGHT
+    if x < len(grid[0]) - 1 and grid[y][x + 1] == next_altitude:
+        amount_of_directions += 1
+
+    # DOWN
+    if y < len(grid) - 1 and grid[y + 1][x] == next_altitude:
+        amount_of_directions += 1
+
+    # LEFT
+    if x > 0 and grid[y][x - 1] == next_altitude:
+        amount_of_directions += 1
+
+    if amount_of_directions > 1:
+        print(amount_of_directions)
+        print(current_pos_trail_pos)
+
+
+
+def is_junction():
+    global current_pos_junction
+    global current_junction_left_over_directions
+    next_altitude = current_pos_trail_altitude + 1
+    y, x = current_pos_trail_pos
+    # UP
+    if y > 0 and grid[y - 1][x] == next_altitude:
+        current_junction_left_over_directions.append('^')
+    # RIGHT
+    if x < len(grid[0]) - 1 and grid[y][x + 1] == next_altitude:
+        current_junction_left_over_directions.append('>')
+    # DOWN
+    if y < len(grid) - 1 and grid[y + 1][x] == next_altitude:
+        current_junction_left_over_directions.append('v')
+    # LEFT
+    if x > 0 and grid[y][x - 1] == next_altitude:
+        current_junction_left_over_directions.append('<')
+
+    print(current_pos_trail_pos)
+    print(current_junction_left_over_directions)
+    current_junction_left_over_directions = []
+
+    if len(current_junction_left_over_directions) > 1:
+        current_pos_junction = [y, x]
+        print(current_pos_junction)
+        print(current_junction_left_over_directions)
+    else:
+        current_junction_left_over_directions = []
+
+
 
 def print_pos():
     print('found trail')
     print(current_pos_trail_pos)
     print(current_pos_trail_altitude)
-
 
 
 if __name__ == '__main__':
@@ -88,20 +141,23 @@ if __name__ == '__main__':
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 0:
+                print("found zero", y, x)
                 print(0)
                 current_pos = [y, x]
                 current_pos_trail_pos = [y, x]
                 while(True):
+                    is_junction_test()
                     check_up()
+
                     check_right()
+
                     check_down()
+
                     check_left()
+
                     if current_pos_trail_altitude == 9:
                         amount_found_trails += 1
+                        current_pos_trail_altitude = 0
                         break
 
     print('amount found trails: ', amount_found_trails)
-
-
-
-
